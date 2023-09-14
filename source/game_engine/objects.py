e0460ff470bca0f2e6c73315c2d9fdb657187f13
Liam <liam.duffell@icloud.com>
2023-09-14 11:16:28 +0100

    started modelling wheel

--- source/game_engine/objects.py
+++ source/game_engine/objects.py
@@ -6,22 +6,33 @@ GRIP = 0.8  # to be expanded on
 
 
 class Wheel:
-    def __init__(self, grip=GRIP, radius=None, torque=None):
+    def __init__(self, grip=GRIP):
+        """Class represents a car wheel"""
         self.grip = grip
-        self.radius = radius  # m
-        self.torque = torque  # N m
+    
+class RearWheel(Wheel):
+    def __init__(self, grip=GRIP, force_applied=None):
+        super.__init__(self, grip)
+        self.force_applied = force_applied
+        
+        
 
-
-    @property
-    def moment_of_inertia(self):
-        return (10 * self.radius**2) / 2
-    # 1/12 * mass * (height**2 + 3 * self.radius**2) perpendicular
-
-
-    @property
-    def angular_velocity(self):  # rad s^-1
-        return self.torque * self.moment_of_inertia
-
+    # @property
+    # def moment_of_inertia(self):
+    #     """Moment of inertia (kg m^2).
+    #      Calculated using 1/2 * m * r^2, where m = mass, r = radius."""
+    #     return (10 * self.radius**2) / 2  # kg m^2
+    #
+    # @property
+    # def angular_momentum(self):
+    #     """Angular momentum ("""
+    #     return
+    #
+    # @property
+    # def angular_velocity(self):  # rad s^-1
+    #     """Angular velocity (rad s^-1).
+    #     Calculated using T * I"""
+    #     return self.torque * self.moment_of_inertia
 
     @property
     def linear_velocity(self):  # m s^-1
